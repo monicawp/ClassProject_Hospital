0798b6df64384e8e64e683242bdb129bc8531d10
monicawp <monicawperezf@gmail.com>
2017-10-12 13:31:06 -0400

    first commit

---
+++ ClassProject_Hospital/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProject_Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
